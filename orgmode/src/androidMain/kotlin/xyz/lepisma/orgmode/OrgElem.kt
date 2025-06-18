bab7e1eb765f87340bcc418039b9e1b6e4031588
Abhinav Tushar <abhinav@lepisma.xyz>
2025-06-18 15:01:31 +0530

    Add missing files from modules refactor

---
+++ orgmode/src/androidMain/kotlin/xyz/lepisma/orgmode/OrgElem.kt
@@ -0,0 +1,64 @@
+package xyz.lepisma.orgmode
+
+import xyz.lepisma.orgmode.lexer.Token
+
+/**
+ * Any parsed org element which keeps tracks of the tokens used in parsing
+ */
+sealed interface OrgElem {
+    // We allow changing tokens here so that tree manipulation is easy
+    var tokens: List<Token>
+}
+
+/**
+ * Options that could go in top of the file, code block headers, etc.
+ */
+data class OrgOptions(
+    val map: Map<String, String>,
+    override var tokens: List<Token>
+) : OrgElem
+
+/**
+ * Unlike simple options, org properties could have full fledged org-mode text
+ */
+data class OrgProperties(
+    val map: Map<String, OrgLine>,
+    override var tokens: List<Token>
+) : OrgElem
+
+/**
+ * Tags for files or headings or anywhere else
+ */
+data class OrgTags(
+    val tags: List<String>,
+    override var tokens: List<Token>
+) : OrgElem
+
+data class OrgTableRow(
+    val cells: List<OrgLine>,
+    override var tokens: List<Token>
+) : OrgElem
+
+/**
+ * A single line string with Org Mode formatting enabled
+ */
+data class OrgLine(
+    val items: List<OrgInlineElem>,
+    override var tokens: List<Token>
+) : OrgElem
+
+/**
+ * Represents parsing error of all kinds
+ */
+data class OrgParsingError(
+    val message: String,
+    override var tokens: List<Token> = emptyList()
+) : OrgElem
+
+/**
+ * Represents a plain token parse
+ */
+data class OrgToken(
+    override var tokens: List<Token>
+) : OrgElem
+
